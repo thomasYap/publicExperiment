a5efa706ced7085f4c7d89ca0585f65154246108
JOBST\thomasyap <thomasyap@seekasia.com>
2017-03-10 16:30:41 +0800

    NodeJS Server Test App

---
+++ main.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer( function (request, response) {
+  response.writeHeader(200, {'Content-Type': 'text/plain'});
+
+  response.end('hey you!');
+
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
